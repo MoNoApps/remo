c969492d7355e8ebccad951340ceaf1d6fb76400
Daniel Martinez <dmartz86@gmail.com>
2015-04-20 17:56:46 -0500

    Progress on init script

--- api/response.js
+++ api/response.js
@@ -2,7 +2,11 @@ var response = function(com) {
   com.etime = new Date().getTime();
   com.ttime = com.etime - com.stime;
   console.log( com.ttime );
-  com.db.close();
+  if(com){
+    if(com.db){
+      com.db.close();
+    }
+  }
 };
 
 module.exports = response;
