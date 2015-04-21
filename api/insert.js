844c9a843fe39de5f202155fbd4d05d4e8c808df
Daniel Martinez <dmartz86@gmail.com>
2015-04-21 14:15:46 -0500

    Cleaning ReMo original idea

--- api/insert.js
+++ api/insert.js
@@ -1,23 +1,9 @@
-var insert = function(props) {
-  this.connect( this.merge ( this.props, props ), function(com) {
-    if(!com){
-      return console.log('Com not found.');
-    }
-
-    if(!com.db) {
-      com.err = 'No db object found';
-      if(!com.cb){
-        return console.log('Callback not found.');
-      }
-      return com.cb(com);
-    }
-    var col = com.db.collection(com.collection);
-    col.insert(com.doc, function(err, results) {
-      //com.db.close();
-      com.err = err || false;
-      com.results = results || false;
-      com.cb(com);
-    });
+var insert = function(com) {
+  var col = com.db.collection(com.collection);
+  col.insert(com.doc, function(err, results) {
+    com.err = err || false;
+    com.results = results || false;
+    com.cb(com);
   });
 };
 
--- api/response.js
+++ api/response.js
@@ -1,7 +1,7 @@
 var response = function(com) {
   com.etime = new Date().getTime();
   com.ttime = com.etime - com.stime;
-  console.log( com.ttime );
+  //console.log( com.ttime );
   if(com){
     if(com.db){
       com.db.close();
