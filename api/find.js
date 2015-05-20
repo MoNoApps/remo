b2c0310d0f15fb37bb5f41fdf4c0fdd9cde200a9
Daniel Martinez <dmartz86@gmail.com>
2015-05-20 16:49:00 -0500

    Add find and update with dinamic call for remo functions

--- api/find.js
+++ api/find.js
@@ -1,13 +1,10 @@
-var find = function(props) {
-  this.connect(this.merge( this.props, props ), function(com) {
-    var col = com.db.collection( com.collection );
-    col.find(com.query, com.project, com.options)
-       .limit(com.limit)
-       .sort(com.sort)
-       .toArray(function(err, results) {
-          com.db.close();
-          com.cb(err, results);
-    });
+var find = function(db, message, cb) {
+  var col = db.collection(message.collection);
+  col.find(message.query, message.project, message.options)
+     .limit(message.limit)
+     .sort(message.sort)
+     .toArray(function(err, results) {
+       cb(err, results);
   });
 };
 
--- api/update.js
+++ api/update.js
@@ -1,11 +1,10 @@
-var update = function(props) {
-  this.connect(this.merge( this.props, props ), function(com){
-    var col = com.db.collection(com.collection);
-    col.update(com.query, com.doc, com.concern, function(err, results) {
-      com.db.close();
-      com.cb(err, results);
-    });
-  });
+var update = function(db, message, cb) {
+  var col = db.collection(message.collection);
+  col.update(message.query, message.doc, message.concern,
+    function(err, results) {
+      cb(err, results);
+    }
+  );
 };
 
 module.exports = update;
