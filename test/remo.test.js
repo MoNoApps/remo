2cf25df9e4609cbc38592484122f200cb72a185c
Daniel Martinez <dmartz86@gmail.com>
2015-06-10 02:23:17 +0000

    Add circle testing

---
+++ test/remo.test.js
@@ -0,0 +1,11 @@
+var remo = require('../index');
+
+describe('remo', function(){
+
+  describe('lib', function(){
+    it('find should exists', function(){
+      assert.ok(remo.find);
+    });
+  });
+
+});
