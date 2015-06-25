f7e3e9c9a22e9bf007121c623a0fdc4be90ffc15
Daniel Martinez <dmartz86@gmail.com>
2015-06-25 13:11:26 -0500

    Add create test. Add unknow api function eval.

---
+++ test/unknown.test.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+var pool = require('../api/pool');
+
+describe('remo lib', function(){
+
+  it('unknow', function(done) {
+    pool({
+      collection: 'mine',
+      action: 'unknow'
+    }, function(err, res){
+      assert.equal("Non processable attempt", err);
+      assert.equal(null, res);
+
+      done();
+    });
+  });
+
+});
